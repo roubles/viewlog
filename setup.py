522059dc984562d211bec4f6b1f05c65a8eecc39
rouble <rouble@gmail.com>
2016-01-24 15:35:12 -0500

    setting up version 1.1.3

--- setup.py
+++ setup.py
@@ -3,6 +3,8 @@
 from setuptools import setup,Command
 import os
 
+__version__ = '1.1.3'
+
 class DocGenerator(Command):
     description = "Convert README.md to README.txt"
     user_options = []
@@ -18,10 +20,11 @@ class DocGenerator(Command):
     
 setup(
     name='viewlog',
-    version='1.1.2',
+    version=__version__,
     author='roubles',
     author_email='rouble@gmail.com',
     url='https://github.com/roubles/viewlog',
+    download_url='https://github.com/roubles/viewlog/tarball/' + __version__,
     license='Creative Commons Attribution-Noncommercial-Share Alike license',
     description='terminal git log browser',
     long_description=open('README.txt').read(),
