46f62bcf50e237af042981c2b641360cc7a09e14
rouble <rouble@gmail.com>
2016-01-26 13:59:53 -0500

    added support for multiple editors, fixed bugs

--- setup.py
+++ setup.py
@@ -1,23 +1,41 @@
 #!/usr/bin/env python
 
-from setuptools import setup,Command
 import os
+import shutil
+from setuptools import setup,Command
 
 __version__ = '1.1.3'
 
-class DocGenerator(Command):
-    description = "Convert README.md to README.txt"
+class Doc(Command):
+    description = "Custom doc command that converts README.md to the reStructured text file README.txt"
     user_options = []
     def initialize_options(self):
-        pass
+        self.cwd = None
     def finalize_options(self):
-        pass
+        self.cwd = os.getcwd()
     def run(self):
         import pypandoc
+        assert os.getcwd() == self.cwd, 'Must be in package root: %s' % self.cwd
         description = pypandoc.convert('README.md', 'rst')
         with open('README.txt','w+') as f:
             f.write(description)
-    
+
+class Clean(Command):
+    description = "Custom clean command that forcefully removes dist/build directories"
+    user_options = []
+    def initialize_options(self):
+        self.cwd = None
+    def finalize_options(self):
+        self.cwd = os.getcwd()
+    def run(self):
+        assert os.getcwd() == self.cwd, 'Must be in package root: %s' % self.cwd
+        shutil.rmtree("./build", ignore_errors=True)
+        shutil.rmtree("./dist", ignore_errors=True)
+        shutil.rmtree("./viewlog.egg-info", ignore_errors=True)
+        try:
+            os.remove("./MANIFEST")
+        except OSError:
+            pass
 setup(
     name='viewlog',
     version=__version__,
@@ -29,6 +47,6 @@ setup(
     description='terminal git log browser',
     long_description=open('README.txt').read(),
     scripts = ['scripts/viewlog'],
-    install_requires=['pick>=0.4.0'],
-    cmdclass={ 'doc': DocGenerator }
+    install_requires=['pick==0.4.0'],
+    cmdclass={ 'doc': Doc, 'clean': Clean}
 )
